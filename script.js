8e5cc02f0db96abc081c58be661b5befb293753b
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-22 12:10:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,36 +1,15 @@
-// Check if user preferences exist in cookies
-const fontSizeCookie = getCookie('fontSize');
-const fontColorCookie = getCookie('fontColor');
+// Check if user preferences exist in local storage
+const fontSizeStorage = localStorage.getItem('fontSize');
+const fontColorStorage = localStorage.getItem('fontColor');
 
-if (fontSizeCookie) {
-  document.documentElement.style.fontSize = fontSizeCookie;
-  document.getElementById('fontsize').value = fontSizeCookie;
+if (fontSizeStorage) {
+  document.documentElement.style.fontSize = fontSizeStorage;
+  document.getElementById('fontsize').value = fontSizeStorage;
 }
 
-if (fontColorCookie) {
-  document.documentElement.style.color = fontColorCookie;
-  document.getElementById('fontcolor').value = fontColorCookie;
-}
-
-// Function to get cookie value by name
-function getCookie(name) {
-  const cookies = document.cookie.split(';');
-
-  for (let i = 0; i < cookies.length; i++) {
-    const cookie = cookies[i].trim();
-    if (cookie.startsWith(name + '=')) {
-      return cookie.substring(name.length + 1);
-    }
-  }
-
-  return null;
-}
-
-// Function to set cookie
-function setCookie(name, value, days) {
-  const expires = new Date();
-  expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-  document.cookie = `${name}=${value};expires=${expires.toUTCString()}`;
+if (fontColorStorage) {
+  document.documentElement.style.color = fontColorStorage;
+  document.getElementById('fontcolor').value = fontColorStorage;
 }
 
 // Handle form submission
@@ -40,11 +19,12 @@ document.querySelector('form').addEventListener('submit', function (e) {
   const fontSize = document.getElementById('fontsize').value + 'px';
   const fontColor = document.getElementById('fontcolor').value;
 
-  // Set preferences in cookies
-  setCookie('fontSize', fontSize, 30);
-  setCookie('fontColor', fontColor, 30);
+  // Set preferences in local storage
+  localStorage.setItem('fontSize', fontSize);
+  localStorage.setItem('fontColor', fontColor);
 
   // Apply preferences to the page
   document.documentElement.style.fontSize = fontSize;
   document.documentElement.style.color = fontColor;
 });
+
