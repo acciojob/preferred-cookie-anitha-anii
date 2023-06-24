540493ebd5e2b9b467e138b4ffa48eacd4c2b003
Anitha chowdary <130220172+anitha-anii@users.noreply.github.com>
2023-06-24 16:25:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,61 @@
 // Check if user preferences exist in local storage
-const fontSizeStorage = localStorage.getItem('fontSize');
-const fontColorStorage = localStorage.getItem('fontColor');
-
-if (fontSizeStorage) {
-  document.documentElement.style.fontSize = fontSizeStorage;
-  document.getElementById('fontsize').value = fontSizeStorage;
+// Function to set cookie
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + value + expires + "; path=/";
 }
 
-if (fontColorStorage) {
-  document.documentElement.style.color = fontColorStorage;
-  document.getElementById('fontcolor').value = fontColorStorage;
+// Function to get cookie
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
 }
 
-// Handle form submission
-document.querySelector('form').addEventListener('submit', function (e) {
-  e.preventDefault();
+// Function to apply user preferences
+function applyPreferences() {
+  var fontSize = getCookie("fontSize");
+  var fontColor = getCookie("fontColor");
 
-  const fontSize = document.getElementById('fontsize').value + 'px';
-  const fontColor = document.getElementById('fontcolor').value;
+  if (fontSize) {
+    document.body.style.fontSize = fontSize + "px";
+    document.getElementById("fontsize").value = fontSize;
+  }
 
-  // Set preferences in local storage
-  localStorage.setItem('fontSize', fontSize);
-  localStorage.setItem('fontColor', fontColor);
+  if (fontColor) {
+    document.body.style.color = fontColor;
+    document.getElementById("fontcolor").value = fontColor;
+  }
+}
 
-  // Apply preferences to the page
-  document.documentElement.style.fontSize = fontSize;
-  document.documentElement.style.color = fontColor;
+// Apply preferences on page load
+window.onload = function() {
+  applyPreferences();
+};
+
+// Event listener for form submission
+document.querySelector("form").addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent form submission
+
+  // Get user preferences
+  var fontSize = document.getElementById("fontsize").value;
+  var fontColor = document.getElementById("fontcolor").value;
+
+  // Set preferences as cookies
+  setCookie("fontSize", fontSize, 365);
+  setCookie("fontColor", fontColor, 365);
+
+  // Apply preferences
+  applyPreferences();
 });
 
